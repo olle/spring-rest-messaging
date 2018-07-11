9fb2300a5934093a8475934698a0171785dcc877
Olle Törnström <olle@studiomediatech.com>
2018-07-11 20:58:10 +0200

    Runnin' with undertow

---
+++ src/main/java/com/studiomediatech/rmsg/RestMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.studiomediatech.rmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMessagingApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(RestMessagingApplication.class, args);
+		Thread.currentThread().join();
+	}
+}
